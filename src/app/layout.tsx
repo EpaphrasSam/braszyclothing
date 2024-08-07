5548a0a471d46ed4e0ab32b2b3ee973aeb05ee55
EpaphrasSam <isinesam@gmail.com>
2024-08-07 16:16:23 +0000

    Added trustpilot to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,16 @@ export default function RootLayout({
               src="https://cdn.promotekit.com/promotekit.js"
               data-promotekit="ab283b38-496d-4103-9794-5e9ecced440d"
             ></Script>
+            <Script
+              id="trustpilot-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+                a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+                f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+                tp('register', 'CbshEyqQHHTu288h');`,
+              }}
+            />
             <main>{children}</main>
           </Providers>
         </SessionProvider>
