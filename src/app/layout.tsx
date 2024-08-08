5a42a19f4606a0eb73a8a1f31f17137a2b665afc
EpaphrasSam <isinesam@gmail.com>
2024-08-08 10:54:31 +0000

    added caching back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const dynamic = "force-dynamic";
-
 export default function RootLayout({
   children,
 }: {
@@ -38,7 +36,7 @@ export default function RootLayout({
             ></Script>
             <Script
               id="trustpilot-script"
-              strategy="beforeInteractive"
+              strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
                 a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
