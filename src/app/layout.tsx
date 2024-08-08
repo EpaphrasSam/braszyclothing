5212021801809f9fcd35fd7490991aa8a53a07d3
EpaphrasSam <isinesam@gmail.com>
2024-08-08 10:43:10 +0000

    Changed strategy of script to beforeInteractive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             ></Script>
             <Script
               id="trustpilot-script"
-              strategy="afterInteractive"
+              strategy="beforeInteractive"
               dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
                 a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
